89e2fa713a88ae8fd799b48b570a038c06cfacac
Andde <andde.indaburu@gmail.com>
2022-01-17 16:58:16 +0100

    setup
    adding setup e

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(name='Tgv project',
+      version='0.0',
+      description='Scrapper TGVmax',
+      author='Etienne et Andde',
+      author_email='andde.indaburu@gmail.com',
+      url='https://github.com/aindaburu/tgv-project',
+      packages=[],
+     )
+
