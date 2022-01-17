6f3ef5f782f016fd7d4d9d536dcfcbb21c9bb390
Andde <andde.indaburu@gmail.com>
2022-01-17 17:44:44 +0100

    url requests

--- src/tgv_project/main.py
+++ src/tgv_project/main.py
@@ -1,2 +1,8 @@
-def hello():
-    print('Hello world!')
+import requests
+
+URL = 'https://data.sncf.com/api/records/1.0/search/?dataset=tgvmax&q=&facet=date&facet=origine&facet=destination&facet=od_happy_card'
+
+def request_tgvmax():
+    r = requests.get(URL)
+    print(r.json())
+
