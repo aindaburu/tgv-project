93a2147eca4116206ad57f8c354ecc7edc21b160
Andde <andde.indaburu@gmail.com>
2022-01-19 14:51:45 +0100

    lint modifications

--- src/tgv_project/scrapper.py
+++ src/tgv_project/scrapper.py
@@ -1,15 +1,26 @@
+"""
+This code aims at scrapping real-time data from tgvmax.com.
+"""
+
 import requests
 
 API_URL = 'https://data.sncf.com/api/records/1.0'
 
+
 def generate_search_url(date, origine, destination):
+    """
+    Scrapping data from API_URL.
+    """
     url = f"{API_URL}/search/?dataset=tgvmax&q=&facet=date&facet=origine&facet=destination&" \
           f"refine.origine={origine}&refine.date={date}&refine.destination={destination}"
-    new_url = url.replace(" ","+")
+    new_url = url.replace(" ", "+")
     return new_url
 
-def scrap_trip(date, origine, destination):
-    search_url = generate_search_url(date, origine, destination)
-    r = requests.get(search_url)
-    return r.json()
 
+def scrap_trip(date, origine, destination):
+    """
+    Making the HTTP request.
+    """
+    search_url = generate_search_url(date, origine, destination)
+    response = requests.get(search_url)
+    return response.json()
--- test/test_scrapper.py
+++ test/test_scrapper.py
@@ -1,9 +1,23 @@
+"""
+Test all the functions.
+"""
+
 from tgv_project.scrapper import scrap_trip, generate_search_url
 
+
 def test_generate_search_url():
+    """
+    Test of url search.
+    """
     url = generate_search_url("2022-01-31", "BAYONNE", "PARIS (intramuros)")
-    assert url == "https://data.sncf.com/api/records/1.0/search/?dataset=tgvmax&q=&facet=date&facet=origine&facet=destination&refine.origine=BAYONNE&refine.date=2022-01-31&refine.destination=PARIS+(intramuros)"
+    assert url == "https://data.sncf.com/api/records/1.0/search/?dataset=tgvmax&q=&facet=date&" \
+                  "facet=origine&facet=destination&refine.origine=BAYONNE&refine.date=2022-01-31&" \
+                  "refine.destination=PARIS+(intramuros)"
+
 
 def test_scrap_trip():
+    """
+    Test of data scrapper.
+    """
     result = scrap_trip("2022-01-31", "BORDEAUX ST JEAN", "PARIS (intramuros)")
     assert result['nhits'] > 0
